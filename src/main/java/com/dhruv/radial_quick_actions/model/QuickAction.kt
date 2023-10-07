1bad895fa334cf59d5d997eba2bee9fa0ad51a52
Dhruv Bansal <66933165+MRfantastic3DGamer@users.noreply.github.com>
2023-10-07 15:49:24 +0530

    Update QuickAction.kt

--- src/main/java/com/dhruv/radial_quick_actions/model/QuickAction.kt
+++ src/main/java/com/dhruv/radial_quick_actions/model/QuickAction.kt
@@ -1,5 +1,8 @@
 package com.dhruv.radial_quick_actions.model
 
+/**
+ * the base class, children of this class are passed around
+ */
 abstract class QuickAction(
     val name: String,
     val onSelect: (()->Unit)?
